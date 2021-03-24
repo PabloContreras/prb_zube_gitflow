edc2da5020149f42fcfa7c4be1ddfe3549fdecc9
pablocontreras <pablo_contreras_1997@outlook.com>
2021-03-24 09:44:47 -0600

    Little fix close #3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Holis
+                    Holis :)
                 </div>
 
                 <div class="links">
